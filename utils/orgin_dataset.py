8faaec27a6ced9c006fe001ad3cd70d4a018b7b3
WithoutOcean <91295772+WithoutOcean@users.noreply.github.com>
2025-05-21 22:26:04 +0800

    Update orgin_dataset.py

--- utils/orgin_dataset.py
+++ utils/orgin_dataset.py
@@ -1,49 +1,30 @@
-import glob
-import json
+from utils.utils import (DEFAULT_IM_END_TOKEN, DEFAULT_IM_START_TOKEN,
+                         DEFAULT_IMAGE_PATCH_TOKEN,DEFAULT_IMAGE_VISION_START,
+DEFAULT_IMAGE_VISION_END,
+DEFAULT_IMAGE_PAD,
+)
 import os
 import random
-import tifffile as tf 
+import json
 import cv2
 import numpy as np
 import torch
 import torch.nn.functional as F
-from PIL import Image
-from pycocotools.coco import COCO
+from pycocotools import mask
 from transformers import CLIPImageProcessor
-
+import glob
 from model.llava import conversation as conversation_lib
 from model.segment_anything.utils.transforms import ResizeLongestSide
-
+from PIL import Image
 from .utils import ANSWER_LIST, SHORT_QUESTION_LIST
-
-
-sample_ratio=0.7
+import re
 def init_farmsegvl(base_image_dir):
-    # 取classes
-    classes = []
-    with open("./utils/CLASS_NAME") as f:
-        for line in f.readlines():
-            classes.append(line.strip().split(": ")[-1])
-    classes = np.array(classes)
-    images = []
-    # 取label路径
-    labels = glob.glob(
-        os.path.join(base_image_dir, "train", "lbl", "*.png")
-    )
-    # 取image路径
-    images = [
-        x.replace("lbl", "img") for x in labels
-    ]
-    ratio=int(len(images)*sample_ratio)
-    # print(' ratio: ', ratio)
-    return classes, images[:ratio], labels[:ratio]
 
-def gdal_loader(img_path, mask_path):
-    img = tf.imread(img_path)
-    mask = tf.imread(mask_path)
-
-    img = img / 255.0  
-    return img, mask
+    images=glob.glob(os.path.join(base_image_dir,"train","img","*.png"))
+    masks=[x.replace("img","lbl") for x in images]
+    texts=[x.replace("img","json") for x in images]
+    texts=[x.replace(".png",".json") for x in texts]
+    return texts, images, masks
 
 class orginDataset(torch.utils.data.Dataset):
     pixel_mean = torch.Tensor([123.675, 116.28, 103.53]).view(-1, 1, 1)
@@ -61,7 +42,7 @@ class orginDataset(torch.utils.data.Dataset):
         image_size: int = 224,
         num_classes_per_sample: int = 3,
         exclude_val=False,
-        orgin_data="FIT",
+        sem_seg_data="farmsegvl",
     ):
         self.exclude_val = exclude_val
         self.samples_per_epoch = samples_per_epoch
@@ -72,26 +53,17 @@ class orginDataset(torch.utils.data.Dataset):
         self.tokenizer = tokenizer
         self.precision = precision
         self.transform = ResizeLongestSide(image_size)
-        self.clip_image_processor = CLIPImageProcessor.from_pretrained(vision_tower)
+        #self.clip_image_processor = CLIPImageProcessor.from_pretrained(vision_tower)
 
         self.short_question_list = SHORT_QUESTION_LIST
         self.answer_list = ANSWER_LIST
-
         self.data2list = {}
-        self.data2classes = {}
-
-        self.sem_seg_datas = orgin_data
+        self.sem_seg_datas = sem_seg_data
         ds =self.sem_seg_datas
-        classes, images, labels = eval("init_{}".format(ds))(base_image_dir)
-        self.data2list[ds] = (images, labels)
-        self.data2classes[ds] = classes
+        text, images, labels = eval("init_{}".format(ds))(base_image_dir)
+        self.data2list[ds] = (text, images, labels)
         self.length=len(images)
-    
-        if ds in self.sem_seg_datas:
-            self.hunan_class2index = {
-                c: i for i, c in enumerate(self.data2classes[ds])
-            }
-
+        print('len(images):',len(images))
     def __len__(self):
         return self.samples_per_epoch
 
@@ -109,55 +81,23 @@ class orginDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         ds = self.sem_seg_datas
-        image, labels = self.data2list[ds]
-        # print("seg: ", len(image))
-        idx = random.randint(0, len(image) - 1)
-        image_path = image[idx]
+        images, labels,texts = self.data2list[ds]
+        # print("refer: ", len(image))
+        idx = random.randint(0, len(images) - 1)
+        image_path = images[idx]
         label_path = labels[idx]
-
-        label = Image.open(label_path)
-        label = np.array(label)
-        label[label == 0] = 255
-        img = cv2.imread(image_path)
-        image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        # preprocess image for clip
-        image_clip = self.clip_image_processor.preprocess(
-            image, return_tensors="pt"
-        )["pixel_values"][0]
-        image = self.transform.apply_image(image)  # preprocess image for sam
-        resize = image.shape[:2]
-        unique_label = np.unique(label).tolist()
-        if 255 in unique_label:
-            unique_label.remove(255)
-        if len(unique_label) == 0:
-            return self.__getitem__(0)
-
-        classes = [self.data2classes[ds][class_id] for class_id in unique_label]
-        
-        if len(classes) >= self.num_classes_per_sample:
-            sampled_classes = np.random.choice(
-                 classes, size=self.num_classes_per_sample, replace=False
-            ).tolist()
-        else:
-            sampled_classes = classes
-            
+        text_path=texts[idx]
+        f = open(text_path, encoding = 'utf-8')
+        path=f.read()
+        text =json.loads(path)['img_description_eg']
+        sampled_classes=text
         questions = []
         answers = []
-        class_ids = []
-        for sampled_cls in sampled_classes:
-            text = sampled_cls
-
-            assert len(text.split("||")) == 1
-            question_template = random.choice(self.short_question_list)
-            questions.append(question_template.format(class_name=text.lower()))
-
-            answers.append(random.choice(self.answer_list))
-
-            if ds in ["paco_lvis", "pascal_part"]:
-                continue
-
-            class_id = self.data2classes[ds].tolist().index(sampled_cls)
-            class_ids.append(class_id)
+        text = text.strip()
+        assert len(text.split("||")) == 1
+        question_template = random.choice(self.short_question_list)
+        questions.append(question_template.format(text_name=text,class_name="farmland"))
+        answers.append(random.choice(self.answer_list))
 
         conversations = []
         conv = conversation_lib.default_conversation.copy()
@@ -170,13 +110,26 @@ class orginDataset(torch.utils.data.Dataset):
             conversations.append(conv.get_prompt())
             i += 1
 
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # preprocess image for clip
+        image_clip = self.clip_image_processor.preprocess(image, return_tensors="pt")[
+            "pixel_values"
+        ][0]
+
+        image = self.transform.apply_image(image)  # preprocess image for sam
+        resize = image.shape[:2]
         image = self.preprocess(torch.from_numpy(image).permute(2, 0, 1).contiguous())
 
-        label = torch.from_numpy(label).long()
-        masks = []
-        for class_id in class_ids:
-            masks.append(label == class_id)
-        masks = torch.stack(masks, dim=0)
+        flag = False
+        mask = Image.open(label_path)
+        masks = np.array(mask)
+        # masks=masks.max(axis=2)
+        masks[masks!=0]=1
+        masks =  masks[np.newaxis,:,:]
+        # masks = np.stack(masks, axis=0)
+        masks = torch.from_numpy(masks)
+        label = torch.ones(masks.shape[1], masks.shape[2]) * self.ignore_label
         return (
             image_path,
             image,
